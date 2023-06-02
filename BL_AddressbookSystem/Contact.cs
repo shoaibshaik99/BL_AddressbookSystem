b746138b6f265c07515885d10bb3924a978e8efe
shaikshoaib99 <smdshoaib137@gmail.com>
2023-06-02 16:02:10 +0530

    [Shoaib] Adding a contact class

---
+++ BL_AddressbookSystem/Contact.cs
@@ -0,0 +1,25 @@
+﻿namespace BL_AddressbookSystem
+{
+    internal class Contact
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string zipCode;
+        public string phoneNumber;
+        public string email;
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+}
--- BL_AddressbookSystem/Program.cs
+++ BL_AddressbookSystem/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Program!");
+            //New contact can be created here, in this use case just added a contact class that can be used
+            //to create contact in upcoming use cases.
+
         }
     }
 }
